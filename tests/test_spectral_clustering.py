422021144f2763a5021e4315d7f3b9add018fbcd
Toliak <toliakpurple@gmail.com>
2019-11-25 11:31:06 +0300

    Add tests

--- tests/test_spectral_clustering.py
+++ tests/test_spectral_clustering.py
@@ -16,10 +16,10 @@ def demo_image():
     """
     from core import Dataset
 
-    return Dataset.load_by_path('multiple_gray.png')
+    return Dataset.load_by_path('test_cluster.png')
 
 
-def test_threshold_demo_image_availability(demo_image):
+def test_spectral_clustering_demo_image_availability(demo_image):
     """Проверка доступности демонстрационного изображения
     """
     assert demo_image is not None
